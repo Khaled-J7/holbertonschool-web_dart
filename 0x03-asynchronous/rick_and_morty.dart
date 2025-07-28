f713a95da4a6d7f2ee3665c909b4d3922baff243
Khaled-J7 <jallouli.khaled.2001@gmail.com>
2025-07-29 00:55:32 +0100

    Replace return with print

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -3,32 +3,25 @@ import 'package:http/http.dart' as http;
 
 Future<void> printRmCharacters() async {
   try {
-    // Start with the first page
     String url = 'https://rickandmortyapi.com/api/character';
 
     while (url.isNotEmpty) {
-      // Make the HTTP request
       final response = await http.get(Uri.parse(url));
 
-      if (response.statusCode == 200) {
-        // Parse the JSON response
-        final data = json.decode(response.body);
-        final characters = data['results'] as List;
-
-        // Print each character's name
-        for (var character in characters) {
-          print(character['name']);
-        }
-
-        // Check if there's a next page
-        final info = data['info'];
-        url = info['next'] ?? '';
-      } else {
-        throw Exception('Failed to load characters: ${response.statusCode}');
+      if (response.statusCode != 200) {
+        throw Exception('HTTP ${response.statusCode}');
       }
+
+      final data = json.decode(response.body);
+      final characters = data['results'] as List;
+
+      for (var character in characters) {
+        print(character['name']);
+      }
+
+      url = data['info']['next'] ?? '';
     }
   } catch (e) {
     print('error caught: $e');
-    return;
   }
 }
