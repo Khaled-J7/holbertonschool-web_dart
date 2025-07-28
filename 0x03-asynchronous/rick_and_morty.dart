cff2250fd76e409fb0b291c8d73d44f858dda105
Khaled-J7 <jallouli.khaled.2001@gmail.com>
2025-07-29 00:38:09 +0100

    Simplify the process

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -3,38 +3,23 @@ import 'package:http/http.dart' as http;
 
 Future<void> printRmCharacters() async {
   try {
-    // 1. Start with the initial API endpoint for characters.
-    String? nextUrl = 'https://rickandmortyapi.com/api/character';
+    final response =
+        await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
 
-    // 2. Loop as long as there is a next page to fetch.
-    while (nextUrl != null) {
-      // 3. Make the HTTP GET request.
-      final response = await http.get(Uri.parse(nextUrl));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
 
-      // 4. Check if the request was successful.
-      if (response.statusCode == 200) {
-        // 5. Decode the JSON response body into a Map.
-        Map<String, dynamic> data = jsonDecode(response.body);
-
-        // 6. The character data is in a list under the 'results' key.
-        List<dynamic> characters = data['results'];
-
-        // 7. Loop through the list of characters and print their names.
-        for (var character in characters) {
-          print(character['name']);
+      if (data['results'] is List) {
+        for (var character in data['results']) {
+          print('${character['name']}');
         }
-
-        // 8. Update nextUrl with the URL for the next page.
-        // If there are no more pages, data['info']['next'] will be null,
-        // and the while loop will terminate.
-        nextUrl = data['info']['next'];
       } else {
-        // If the status code is not 200, throw an error to be caught.
-        throw 'Failed to load characters';
+        print('Invalid API response format.');
       }
+    } else {
+      print('Failed to fetch data. Status code: ${response.statusCode}');
     }
   } catch (error) {
-    // 9. If any error occurs in the try block, catch it and print the message.
-    print('error caught: $error');
+    print('Error caught: $error');
   }
 }
